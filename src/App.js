a960e90b94a1ea420421e7d871d8b54c9f96ced9
Sofia <sgandsteph@gmail.com>
2021-01-09 00:45:03 +0100

    adds roboto font, fixes columns, matches properties values to tinder

--- src/App.js
+++ src/App.js
@@ -6,19 +6,13 @@ import Cat from "./Cat";
 const App = () => {
   return (
     <div className="container">
-      <div className="d-flex justify-content-center">
-        <div className="row">
-          <div className="left-column col-6">
-            <div id="header">
-              <h1>Kitty Tinder</h1>
-            </div>
-              <button id="play">Play</button>
-          </div>
-            <div className="col-6 p-5">
-              <Cat name= "Trotzky" life_span="4" temperment="Grumpy"/>
-            </div>
-         </div>
-       </div>
+      <div className="left-column">
+        <h1 id="header">Kitty Tinder</h1>
+          <Play />
+      </div>
+      <div className="right-column">
+        <Cat name= "Trotzky" life_span="4" temperment="Grumpy"/>
+      </div>
     </div>
   );
 };
