4afaf0a319e3e5933c68a3b638af0c27b06ac21c
Sofia <sgandsteph@gmail.com>
2021-01-11 02:19:30 +0100

    Uses dog api replacement to test cat

--- src/Cat.jsx
+++ src/Cat.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 
 class Cat extends Component {
-  render() {
-    console.log(this.props.cat);
+  constructor(props) {
+    super(props);
+  } render() {
+    console.log(this.props.cat.message);
   return (
     <div className="card">
       <div className="cat-specs">
-        <h1>{this.props.cat.id ? this.props.cat.id : "None"}</h1>
+      <img src={this.props.cat.message} alt={name} />
+        <h1>{this.props.cat.message ? this.props.cat.message : "None"}</h1>
       </div>
     </div>
-  )};
+  )
+  }
 }
 
 export default Cat;
