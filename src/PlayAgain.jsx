73cd3edb370fddd0d9f8af5d521859e9b1b2f600
Sofia <sgandsteph@gmail.com>
2021-01-18 01:32:05 +0100

    adds cat sub for any cat without an image

--- src/PlayAgain.jsx
+++ src/PlayAgain.jsx
@@ -3,15 +3,10 @@ import React from "react";
 class PlayAgain extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isPlayOn: true};
     this.handlePlayAgain = this.handlePlayAgain.bind(this);
   }
 
   handlePlayAgain() {
-    this.setState(state => ({
-      isPlayOn: !state.isPlayOn
-    }));
-    console.log('test')
     this.props.resetTimer()
     this.props.setTimer()
     this.props.resetArrays()
